a170c682efb9a8618ae2ebfdf274560cfbb8c1ee
Robert Ancell <robert.ancell@gmail.com>
2018-04-07 14:46:35 +1200

    Add copyright header

--- pride.py
+++ pride.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+# Copyright (C) 2018 Robert Ancell
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version. See http://www.gnu.org/copyleft/gpl.html the full text of the
+# license.
+
 import sys
 import os
 import curses
