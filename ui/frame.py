29cd6f73ad98da8ee71672ca2084ebf78f5b6c4c
Robert Ancell <robert.ancell@gmail.com>
2018-04-29 22:26:57 +1200

    Use single quotes for consistency

--- ui/frame.py
+++ ui/frame.py
@@ -12,7 +12,7 @@ class Pixel:
     def __init__ (self):
         self.set_value (ord (' '))
 
-    def set_value (self, character, foreground = "#FFFFFF", background = "#000000"):
+    def set_value (self, character, foreground = '#FFFFFF', background = '#000000'):
         self.character = character
         self.foreground = foreground
         self.background = background
@@ -34,10 +34,10 @@ class Frame:
             self.buffer.append (line)
         self.cursor = (0, 0)
 
-    def clear (self, color = "#000000"):
+    def clear (self, color = '#000000'):
         self.fill (0, 0, self.width, self.height, ord (' '), background = color)
 
-    def fill (self, x, y, width, height, character, foreground = "#FFFFFF", background = "#000000"):
+    def fill (self, x, y, width, height, character, foreground = '#FFFFFF', background = '#000000'):
         height = min (height, self.height)
         width = min (width, self.width)
         for y_ in range (y, height):
@@ -54,7 +54,7 @@ class Frame:
             for x_ in range (width):
                 target_line[x + x_].copy (source_line[x_])
 
-    def render_text (self, x, y, text, foreground = "#FFFFFF", background = "#000000"):
+    def render_text (self, x, y, text, foreground = '#FFFFFF', background = '#000000'):
         if y >= self.height:
             return
         line = self.buffer[y]
@@ -74,10 +74,10 @@ class Frame:
                 return
             line = self.buffer[y + i]
             for (j, c) in enumerate (source_line):
-                (foreground, background) = ("#FFFFFF", "#000000")
+                (foreground, background) = ('#FFFFFF', '#000000')
                 if color_lines is not None:
                     color_code = color_lines[i][j]
-                    (foreground, background) = color_map.get (color_code, ("#FFFFFF", "#000000"))
+                    (foreground, background) = color_map.get (color_code, ('#FFFFFF', '#000000'))
                 if x + j >= self.width:
                     break
                 line[x + j].set_value (ord (c), foreground, background)
